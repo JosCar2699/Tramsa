5691a23f54dcd9fe0b912ac9fb731759b3f063fd
asandovalg768 <43359591+asandovalg768@users.noreply.github.com>
2020-08-20 22:07:34 -0600

    Revert "proyecto"
    This reverts commit f7c6e8b66ab840c10bacccc4638e8ec7f985d94f.

--- NuevoProyecto/prueba/src/routes/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/TRAMSA', {
-        useNewUrlParser: true
-    })
-    .then(db => console.log('Se conectó a la base'))
-    .catch(err => console.error(err));
